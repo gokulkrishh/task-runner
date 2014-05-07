b76c52321c7f44fdd5147c7398bd084f88becda9
sourcebits-gokul <gokulakrishnan.kalaikoven@sourcebits.com>
2014-05-07 17:39:59 +0530

    added default tasks

--- gulpfile.js
+++ gulpfile.js
@@ -4,17 +4,19 @@ var gulp 	  = require('gulp'),
 	chalk 	  = require('chalk'),
 	concat 	  = require('gulp-concat'),
 	jshint 	  = require('gulp-jshint'),
-	// minifycss = require('gulp-minify-css'),
+	minifycss = require('gulp-minify-css'),
 	stylish   = require('jshint-stylish'),
 	uglify    = require('gulp-uglify');
 
+var error = chalk.red.bold,
+	hint  = chalk.yellow.bold;
 
-/**===========================================
-  			JS Hint
-=============================================*/
+/**================================================
+  		Script Tasks -- js hint, uglify, concat
+===================================================*/
 
-gulp.task('jshint', function() {
-	console.log(chalk.yellow.bold('\n --------- JS Hint -------------------------------------------------->>>'));
+gulp.task('scripts', function() {
+	console.log(hint('\n --------- Running script tasks ------------------------------------------->>> \n'));
 	return gulp.src(['app/js/**/*.js']) // 'gulpfile.js'
 	.pipe(jshint('.jshintrc'))
 	.pipe(jshint.reporter(stylish))
@@ -23,17 +25,23 @@ gulp.task('jshint', function() {
 	.pipe(gulp.dest('build/js'));
 });
 
+/**===============================================
+  		CSS Tasks - minify, concat
+=================================================*/
 
-
-
-
-/**===========================================
-  			Gulp default task
-=============================================*/
-
-gulp.task('default', function() {
-	console.log(chalk.yellow.bold('\n --------- Running default task -------------------------------------------------->>>'));
+gulp.task('css', function() {
+	console.log(hint('\n --------- Running CSS tasks ---------------------------------------------->>> \n'));
+	return gulp.src(['app/css/*.css'])
+	.pipe(minifycss())
+	.pipe(concat('styles.css'))
+	.pipe(gulp.dest('build/css'));
 });
+
+/**==============================================
+  		Gulp default task
+=================================================*/
+
+gulp.task('default', ['scripts', 'css']);
 
 
 
