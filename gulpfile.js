6f543abcd2d4742ce46d7762ce23fe8d3ffd1ff4
sourcebits-gokul <gokulakrishnan.kalaikoven@sourcebits.com>
2014-05-07 16:13:55 +0530

    added gulp file

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+"use strict";
+
+var gulp = require('gulp');
+
+
+
+/**===========================================
+  			Gulp default task
+=============================================*/
+
+gulp.task('default', function() {
+	console.log('---- Running default task -------------------------------------------------->>>');
+});
+
+
+
