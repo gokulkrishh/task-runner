71c4fbe5b73d1bdeb3e5913c842b0ec8728624c8
sourcebits-gokul <gokulakrishnan.kalaikoven@sourcebits.com>
2014-05-07 17:16:41 +0530

    added jshint task which uglify and concat js file

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,29 @@
-"use strict";
+'use strict';
 
-var gulp = require('gulp');
+var gulp 	  = require('gulp'),
+	chalk 	  = require('chalk'),
+	concat 	  = require('gulp-concat'),
+	jshint 	  = require('gulp-jshint'),
+	// minifycss = require('gulp-minify-css'),
+	stylish   = require('jshint-stylish'),
+	uglify    = require('gulp-uglify');
+
+
+/**===========================================
+  			JS Hint
+=============================================*/
+
+gulp.task('jshint', function() {
+	console.log(chalk.yellow.bold('\n --------- JS Hint -------------------------------------------------->>>'));
+	return gulp.src(['app/js/**/*.js']) // 'gulpfile.js'
+	.pipe(jshint('.jshintrc'))
+	.pipe(jshint.reporter(stylish))
+	.pipe(uglify())
+	.pipe(concat('all.js'))
+	.pipe(gulp.dest('build/js'));
+});
+
+
 
 
 
@@ -9,7 +32,7 @@ var gulp = require('gulp');
 =============================================*/
 
 gulp.task('default', function() {
-	console.log('---- Running default task -------------------------------------------------->>>');
+	console.log(chalk.yellow.bold('\n --------- Running default task -------------------------------------------------->>>'));
 });
 
 
